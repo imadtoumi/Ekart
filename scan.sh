0e6ef92d8e831129a979474d2e464bcffd73885c
imadtoumi <41326066+imadtoumi@users.noreply.github.com>
2024-08-14 16:46:23 +0100

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,12 @@
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.54.1 image --severity CRITICAL --exit-code 1 --light openjdk:8u151-jdk-alpine3.7
+
+exit_code=$?
+echo "exit code is = $exit_code"
+
+if [ $exit_code == 1 ]
+then
+   echo "Image scanning failed, Image is not secure"
+   exit 1
+else
+   echo "Image scaning passed, Image is secure"
+fi
